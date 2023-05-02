c12e484e378a6aea37af0928575dce7ed4f6fb7a
Sena <117431055+SenaNurzhan@users.noreply.github.com>
2023-05-02 22:42:01 +0600

    Add files via upload

--- templates/db.php
+++ templates/db.php
@@ -1,1 +1,11 @@
+<?php
 
+	$connection = new mysqli("localhost","root","","project_1");
+
+	if($connection->connect_error){
+
+		echo "Error with database connection";
+
+	}
+
+?>
---
+++ templates/header_menu.php
@@ -0,0 +1,33 @@
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="?page=myPage">My Page</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="?page=tweets">Tweets</a>
+                    </li> 
+
+                    <li>
+                        <a href="?page=Settings">Settings</a>
+                    </li> 
+
+                    <li>
+                        <a href="?act=deactive">Deactive</a>
+                    </li>  
+
+                    <li>
+                        <a href="?act=logout">Logout</a>
+                    </li>
+  
+                  
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
